cc0a9d50b6a24e5251cedfa067b33f69a7960573
Juha Louhiranta <juha.louhiranta@anders.fi>
2019-12-19 11:37:41 +0200

    Add tests for running a single importer management command
    Refs AHTI-69

--- features/tests/test_importers.py
+++ features/tests/test_importers.py
@@ -5,12 +5,27 @@ from features.importers.registry import ImporterRegistry
 
 def test_configured_importers_get_called(mocker):
     mocked = []
-
     for cls in ImporterRegistry.registry.values():
-        import_method = mocker.patch.object(cls, "import_features",)
-        mocked.append(import_method)
+        mocked.append(mocker.patch.object(cls, "import_features"))
 
     call_command("import_features")
 
     for import_method in mocked:
         import_method.assert_called_once()
+
+
+def test_running_single_importer(mocker):
+    mhp_identifier = "myhelsinki_places"
+    mhp_mocked = None
+    other_mocked = []
+    for key, cls in ImporterRegistry.registry.items():
+        if key == mhp_identifier:
+            mhp_mocked = mocker.patch.object(cls, "import_features",)
+        else:
+            other_mocked.append(mocker.patch.object(cls, "import_features"))
+
+    call_command("import_features", single=mhp_identifier)
+
+    for import_method in other_mocked:
+        assert not import_method.called
+    mhp_mocked.assert_called_once()
