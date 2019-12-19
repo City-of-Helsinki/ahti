c78d7ae00c71edc2b240f6f38212883e2c0cd76d
Juha Louhiranta <juha.louhiranta@anders.fi>
2019-12-19 14:20:31 +0200

    Test admin_user fixture

--- users/tests/test_user_model.py
+++ users/tests/test_user_model.py
@@ -7,3 +7,10 @@ User = get_user_model()
 @pytest.mark.django_db
 def test_user_model(user):
     assert User.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_staff_user(admin_user):
+    staff = User.objects.first()
+    assert User.objects.count() == 1
+    assert staff.is_staff
