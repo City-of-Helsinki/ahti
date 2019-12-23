416fca458f85191525abb6fe829f984d8f0391ff
Juha Louhiranta <juha.louhiranta@anders.fi>
2019-12-23 16:23:01 +0200

    Add test for graphql Feature id
    Refs AHTI-97

--- features/tests/test_api.py
+++ features/tests/test_api.py
@@ -2,8 +2,10 @@ import pytest
 from django.contrib.gis.geos import Point
 from freezegun import freeze_time
 from graphene.test import Client
+from graphql_relay import to_global_id
 
 from ahti.schema import schema
+from features.schema import Feature
 from features.tests.factories import FeatureFactory, SourceTypeFactory
 
 
@@ -68,3 +70,25 @@ def test_features_query(snapshot, api_client):
     executed = api_client.execute(FEATURES_QUERY)
 
     snapshot.assert_match(executed)
+
+
+FEATURES_ID_QUERY = """
+query FeaturesId {
+  features {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}
+"""
+
+
+def test_features_query_with_ids(api_client):
+    f = FeatureFactory()
+    f_node_id = to_global_id(Feature._meta.name, f.pk)
+
+    executed = api_client.execute(FEATURES_ID_QUERY)
+
+    assert executed["data"]["features"]["edges"][0]["node"]["id"] == f_node_id
