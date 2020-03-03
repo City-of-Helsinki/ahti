ed83fdc7e4d72fb0c0042b5a0a59a872d772e06c
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-03-03 13:11:50 +0200

    Filter Feature admin based on visibility
    Refs AHTI-172

--- features/admin.py
+++ features/admin.py
@@ -6,8 +6,8 @@ from features.models import Feature
 
 @admin.register(Feature)
 class FeatureAdmin(TranslatableAdmin, admin.OSMGeoAdmin):
-    list_display = ("name", "category", "source_type", "source_id")
-    list_filter = ("source_type", "category")
+    list_display = ("name", "category", "source_type", "source_id", "visibility")
+    list_filter = ("source_type", "category", "visibility")
     search_fields = ("translations__name", "source_id")
     ordering = ("translations__name",)
     autocomplete_fields = ("category", "parents")
