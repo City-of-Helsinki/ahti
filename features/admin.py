01841edb1cbcd0002a7cac6b183c58d49ec99bb4
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-03-04 16:14:37 +0200

    Add filters for language to Features admin
    Features can be filtered based on translations on
    django admin. Translations that are available for
    a Feature are also shown.
    
    Refs AHTI-172

--- features/admin.py
+++ features/admin.py
@@ -6,8 +6,20 @@ from features.models import Feature
 
 @admin.register(Feature)
 class FeatureAdmin(TranslatableAdmin, admin.OSMGeoAdmin):
-    list_display = ("name", "category", "source_type", "source_id", "visibility")
-    list_filter = ("source_type", "category", "visibility")
+    list_display = (
+        "name",
+        "category",
+        "source_type",
+        "source_id",
+        "visibility",
+        "language_column",
+    )
+    list_filter = (
+        "source_type",
+        "category",
+        "visibility",
+        "translations__language_code",
+    )
     search_fields = ("translations__name", "source_id")
     ordering = ("translations__name",)
     autocomplete_fields = ("category", "parents")
