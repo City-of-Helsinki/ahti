ea5059ae1ab832b45842e2b9abd74013728e3629
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-06-10 22:57:39 +0300

    Enable container to restore db from a dump
    Restore is done using psql so SQL file is assumed
    as an input. File itself is sent over to the container
    using magic-wormhole.
    
    Dump can be done e.g.:
    pg_dump --clean --if-exists --no-owner --no-acl -f pg.sql postgres://...
    
    Refs AHTI-419

--- .prod/on_deploy.sh
+++ .prod/on_deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
+# Restore a DB dump
+if [[ "$RESTORE_DB_DUMP" = "1" ]]; then
+    echo "Restoring a database dump over the current db..."
+    wormhole receive --hide-progress --accept-file -o /tmp/pg.sql "$RESTORE_DB_DUMP_MWH_CODE"
+    psql "$DATABASE_URL" -f /tmp/pg.sql
+fi
+
 ./manage.py migrate --noinput
